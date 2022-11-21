c978f40a9de089c6c83ad6db697465e90ddd11d8
Franco Pignanelli <franqo1808@gmail.com>
2022-11-21 00:08:41 -0300

    1. rockPaperScissors: finished

--- 1. rockPaperScissors/app.js
+++ 1. rockPaperScissors/app.js
@@ -5,12 +5,15 @@ const possibleChoices = document.querySelectorAll('button');
 
 let userChoice; 
 let computerChoice;
+let result;
+
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
      userChoice = event.target.id;
      userChoiceDisplay.innerHTML = userChoice;
 
      generateComputerChoice();
+     getResult();
 }))
 
 function generateComputerChoice(){
@@ -28,4 +31,33 @@ function generateComputerChoice(){
             computerChoice = 'scissors';
             break;
     }
+
+    computerChoiceDisplay.innerHTML = computerChoice;
 } 
+
+function getResult (){
+    if (userChoice === computerChoice){
+        result = 'Draw';
+    }
+
+    if(userChoice === 'paper' && computerChoice === 'rock'){
+        result = 'Win'
+    }
+    if(userChoice === 'scissors' && computerChoice === 'rock' ){
+        result = 'You lost'
+    }
+    if(userChoice === 'rock' && computerChoice === 'paper' ){
+        result = 'You lost'
+    }
+    if(userChoice === 'scissors' && computerChoice === 'paper'){
+        result = 'Win'
+    }
+    if(userChoice === 'paper' && computerChoice === 'scissors'  ){
+        result = 'You lost'
+    }
+    if(userChoice === 'rock' && computerChoice === 'scissors' ){
+        result = 'Win'
+    }
+
+    resultDisplay.innerHTML = result;
+}
